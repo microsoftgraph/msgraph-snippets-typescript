2c52d8081541ccb3bc5c4f8e5643ba2c289a282e
Jason Johnston <jasonjoh@users.noreply.github.com>
2024-06-07 11:18:34 -0400

    Fix for batch requests

--- src/graphHelper.ts
+++ src/graphHelper.ts
@@ -6,11 +6,16 @@ import {
   DeviceCodeCredential,
   DeviceCodePromptCallback,
 } from '@azure/identity';
-import { Client } from '@microsoft/microsoft-graph-client';
+import {
+  AuthenticationHandler,
+  Client,
+  HTTPMessageHandler,
+} from '@microsoft/microsoft-graph-client';
 // prettier-ignore
 import { TokenCredentialAuthenticationProvider }
   from '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials';
 import { AppConfig } from './appConfig';
+import ClientLoggingMiddleware from './clientLoggingMiddleware';
 
 export function getGraphClientForUser(
   appConfig: AppConfig,
@@ -27,4 +32,30 @@ export function getGraphClientForUser(
   });
 
   return Client.initWithMiddleware({ authProvider: authProvider });
+}
+
+export function getDebugGraphClientForUser(
+  appConfig: AppConfig,
+  deviceCodePrompt: DeviceCodePromptCallback,
+): Client {
+  const credential = new DeviceCodeCredential({
+    clientId: appConfig.clientId,
+    tenantId: appConfig.tenantId,
+    userPromptCallback: deviceCodePrompt,
+  });
+
+  const authProvider = new TokenCredentialAuthenticationProvider(credential, {
+    scopes: appConfig.graphUserScopes,
+  });
+
+  const authHandler = new AuthenticationHandler(authProvider);
+  const loggingHandler = new ClientLoggingMiddleware();
+  const httpHandler = new HTTPMessageHandler();
+
+  authHandler.setNext(loggingHandler);
+  loggingHandler.setNext(httpHandler);
+
+  return Client.initWithMiddleware({
+    middleware: authHandler,
+  });
 }
